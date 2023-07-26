55cc6d207bc64cfe2947e5041a2747bd57c66ba8
Emmanuel <emmanuel@stock2shop.com>
2023-07-26 21:26:30 +0200

    revert

---
+++ Observer/OrderSaveAfter.php
@@ -0,0 +1,102 @@
+<?php
+namespace Stock2Shop\OrderExport\Observer;
+use Magento\Framework\App\Config;
+use Magento\Framework\App\ObjectManager as OM;
+use Magento\Framework\Event\Observer;
+use Magento\Framework\Event\ObserverInterface;
+use Magento\Sales\Api\Data\OrderStatusHistoryInterface as IHistory;
+use Magento\Sales\Model\Order as O;
+use Magento\Sales\Model\Order\Status\History;
+use Magento\Store\Model\ScopeInterface as SS;
+use Magento\Store\Model\Store;
+use Stock2Shop\OrderExport\Payload;
+use Zend_Http_Client as Z;
+// 2018-08-11 Dmitry Fedyuk https://www.upwork.com/fl/mage2pro
+final class OrderSaveAfter implements ObserverInterface {
+	/**
+	 * 2018-08-11
+	 * @override
+	 * @see ObserverInterface::execute()
+	 * @param Observer $ob
+	 */
+	function execute(Observer $ob) {
+		static $in; /** @var bool $in */
+		if (!$in) {
+			$in = true;
+			try {
+				$o = $ob['order']; /** @var O $o */
+				$om = OM::getInstance(); /** @var OM $om */
+				$cfg = $om->get(Config::class); /** @var Config $cfg */
+				if ($cfg->getValue('stock2shop/order_export/enable', SS::SCOPE_STORE, $o->getStore())) {
+					/** @var string $state */ /** @var string $status */
+					list($state, $status) = [$o->getState(), $o->getStatus()];
+					$error_occurred = false;
+					try {
+						$payload = Payload::get($o);
+						$res = self::post($payload, $o->getStore());
+					} /** @var string $res */
+					catch (\Exception $e) {
+						$res = $e->getMessage();
+						$error_occurred = true;
+					}
+					$comment = [
+						"The Stock2Shop's webhook is notified."
+						,"The order's status: «<b>{$status}</b>»."
+						,"The order's state: «<b>{$state}</b>»."
+						,sprintf("The webhook's response: «<b>%s</b>».", mb_substr($res, 0, 25000))
+					];
+					if ($error_occurred) {
+						$comment[] = sprintf("The serialized payload: %s", htmlspecialchars(serialize($payload)));
+					}
+					$h = $o->addStatusHistoryComment(__(
+						implode('<br>', $comment)
+					)); /** @var History|IHistory $h */
+					$h->setIsVisibleOnFront(false);
+					$h->setIsCustomerNotified(false);
+					$h->save();
+				}
+			}
+			finally {$in = false;}
+		}
+	}
+
+	/**
+	 * 2018-08-15
+	 * @param array(string => mixed) $p
+	 * @param Store $s
+	 * @return string
+	 */
+	private static function post(array $p, Store $s) {
+		$om = OM::getInstance(); /** @var OM $om */
+		$cfg = $om->get(Config::class); /** @var Config $cfg */
+		$url = $cfg->getValue('stock2shop/order_export/url', SS::SCOPE_STORE, $s); /** @var string $url */
+		$z = new Z($url, [
+			'accept' => 'application/json'
+			,'timeout' => 120
+			/**
+			 * 2017-07-16
+			 * By default it is «Zend_Http_Client»: @see C::$config
+			 * https://github.com/magento/zf1/blob/1.13.1/library/Zend/Http/Client.php#L126
+			 */
+			,'useragent' => 'Mage2.PRO'
+		]); /** @var Z $r */
+		if (0 === strpos(strtolower($url), 'https') || false !== strpos($url, 'localhost')) {
+			/**
+			 * 2017-07-16
+			 * @see \Zend_Http_Client_Adapter_Socket is the default adapter for Zend_Http_Client:
+			 * @see C::$config https://github.com/magento/zf1/blob/1.13.1/library/Zend/Http/Client.php#L126
+			 * But the adapter can be changed in $config, so we create another adapter.
+			 */
+			$z->setAdapter((new \Zend_Http_Client_Adapter_Socket)->setStreamContext([
+				'ssl' => ['allow_self_signed' => true, 'verify_peer' => false]
+			]));
+		}
+		$z->setMethod(Z::POST);
+		$json = json_encode($p, JSON_PRETTY_PRINT|JSON_UNESCAPED_SLASHES|JSON_UNESCAPED_UNICODE);
+		if (json_last_error() !== JSON_ERROR_NONE) {
+			throw new \Exception("JSON encoding failed: " . json_last_error_msg());
+		}
+		$z->setRawData($json);
+		return $z->request()->getBody();
+	}
+}
